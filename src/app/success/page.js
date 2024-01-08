72ea7c5b42c6bf52b351657e20871741c2577ede
kohcharles <charles@creatual.io>
2024-01-08 22:33:58 +0800

    data&stripe2

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,12 +1,10 @@
 
 import Head from "next/head";
 
-import Hero from '@/app/components/Hero'
 const Success = () => { return (
 <> <Head>
 <title>Success Page</title> </Head>
 <div className="h-screen flex justify-center items-center"> <h1 className="text-white text-4xl">Success</h1>
-<Hero />
 </div> </>
 ); };
 export default Success;
